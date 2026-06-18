950b17ae625cb38c29b67c80af6b0e246dc1a865
DhruvNaina <dhruv4857.be21@chitkara.edu.in>
2026-06-18 18:20:39 +0530

    change

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://13.60.56.187", "http://localhost:5173"],
     credentials: true,
   })
 );
