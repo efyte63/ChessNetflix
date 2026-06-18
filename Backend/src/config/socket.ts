890b52fc2802d75d2df9274a0c0d29eacbe14a6c
DhruvNaina <dhruv4857.be21@chitkara.edu.in>
2026-06-18 16:27:21 +0530

    changed

--- Backend/src/config/socket.ts
+++ Backend/src/config/socket.ts
@@ -1,9 +1,10 @@
 import { Server } from "socket.io";
+import type http from "http";
 import { GameManager } from "../Chess/Gamemanager.js";
 
-let io;
+let io: Server;
 
-export const initSocket = (server) => {
+export const initSocket = (server: http.Server) => {
   io = new Server(server, {
     cors: {
       origin: "http://localhost:5173",
